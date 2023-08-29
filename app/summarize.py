f9cd4f355a968e801fe7b4aefdc846b47a4fe16e
heko <91366057+hkohko@users.noreply.github.com>
2023-08-29 09:54:52 +0700

    refactor for async

--- app/summarize.py
+++ app/summarize.py
@@ -1,33 +1,31 @@
 import spacy
+from asyncio import Runner
+from os import listdir
 from heapq import nlargest
 from spacy.language import Language
 from spacy.tokens.doc import Doc
 from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
 from app.locations import Directories
 
 stopword = list(STOP_WORDS)
 
-with open(Directories.SAMPLE_TEXT.joinpath("text_1.txt"), encoding="utf-8") as file:
-    sample_text = file.read()
 
 nlp: Language = spacy.load("en_core_web_sm")
-docx: Doc = nlp(sample_text)
 
 
-def tokenize_word(docx):
+async def tokenize_word(docx) -> list[str]:
     # token.text = tokenize, and convert to string.
     # allows for string methods to be applied to it
     mytoken = [token.text.lower() for token in docx]
     return mytoken
 
 
-def tokenize_sents(docx: Doc):
+async def tokenize_sentence(docx: Doc) -> list[str]:
     list_of_sentence = [sent.text.lower() for sent in docx.sents]
     return list_of_sentence
 
 
-def remove_stopwords(docx: Doc):
+async def remove_stopwords(docx: Doc) -> list[str]:
     filter_stopword = []
     for word in docx:
         text_token = word.text
@@ -36,7 +34,7 @@ def remove_stopwords(docx: Doc):
     return filter_stopword
 
 
-def get_word_frequency(filter_stopword: list):
+async def get_word_frequency(filter_stopword: list) -> dict[str]:
     word_frequency = {}
     for word in filter_stopword:
         text_token = word.text
@@ -47,7 +45,7 @@ def get_word_frequency(filter_stopword: list):
     return word_frequency
 
 
-def word_weights(word_frequency: dict[str]):
+async def get_word_weights(word_frequency: dict[str]) -> dict[str]:
     word_max_freq = max(word_frequency.values())
     weighted_words = {}
     for word in word_frequency.keys():
@@ -57,33 +55,39 @@ def word_weights(word_frequency: dict[str]):
     return weighted_words
 
 
-def rank_score_sents(docx: Doc, weighted_words: dict[str]):
+async def rank_score_sents(docx: Doc, weighted_words: dict[str]) -> dict[str]:
     score = {}
-    sents_list = tokenize_sents(docx)
-    words_list = tokenize_word(docx)
+    sents_list = await tokenize_sentence(docx)
+    words_list = await tokenize_word(docx)
     for sent in sents_list:
         for word in words_list:
-            if word in weighted_words.keys() and len(sent.split(" ")) < 30:
-                if sent not in score.keys():
-                    score[sent] = weighted_words[word]
-                else:
-                    score[sent] += weighted_words[word]
+            if word in weighted_words.keys() and sent not in score.keys():
+                score[sent] = weighted_words[word]
+            elif word in weighted_words.keys() and sent in score.keys():
+                score[sent] += weighted_words[word]
     return score
 
 
-def nlargest_summary(score: dict):
-    nlargest_summary = nlargest(7, score, key=score.get)
+async def nlargest_summary(score: dict, x_largest: int) -> list:
+    nlargest_summary = nlargest(x_largest, score, key=score.get)
     return nlargest_summary
 
 
-def main(docx: Doc):
-    filtered_stopwords = remove_stopwords(docx)
-    word_freq = get_word_frequency(filtered_stopwords)
-    weighted_words = word_weights(word_freq)
-    score = rank_score_sents(docx, weighted_words)
-    prelim_summary = nlargest_summary(score)
+async def main(docx: Doc):
+    filtered_stopwords = await remove_stopwords(docx)
+    word_freq = await get_word_frequency(filtered_stopwords)
+    weighted_words = await get_word_weights(word_freq)
+    score = await rank_score_sents(docx, weighted_words)
+    prelim_summary = await nlargest_summary(score, 6)
+    print()
     print(prelim_summary)
-
+    print()
 
 if __name__ == "__main__":
-    main(docx)
+    texts = listdir(Directories.SAMPLE_TEXT)
+    for text in texts:
+        with open(Directories.SAMPLE_TEXT.joinpath(text), encoding="utf-8") as file:
+            sample_text = file.read()
+        docx: Doc = nlp(sample_text)
+        with Runner() as runner:
+            runner.run(main(docx))
